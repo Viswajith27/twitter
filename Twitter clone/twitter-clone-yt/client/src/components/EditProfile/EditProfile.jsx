850b05049a77184ccd9c3f94dbff6a8a51438be0
Viswajith Rajan <viswajithrajan@gmail.com>
2023-09-15 16:41:05 +0530

    Update EditProfile.jsx

--- Twitter clone/twitter-clone-yt/client/src/components/EditProfile/EditProfile.jsx
+++ Twitter clone/twitter-clone-yt/client/src/components/EditProfile/EditProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../../redux/axiosconfig";
 import { useDispatch, useSelector } from "react-redux";
 
 import { changeProfile, logout } from "../../redux/userSlice";
