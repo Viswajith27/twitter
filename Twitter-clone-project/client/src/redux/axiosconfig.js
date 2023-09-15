9adcd204da2b3f7f0b70b7aec4aae5508103e638
Viswajith Rajan <viswajithrajan@gmail.com>
2023-09-15 22:49:31 +0530

    Update axiosconfig.js

--- Twitter-clone-project/client/src/redux/axiosconfig.js
+++ Twitter-clone-project/client/src/redux/axiosconfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://twitterrr.onrender.com', // Set your base URL here
+  baseURL: 'https://twitterrr.onrender.com/api', // Set your base URL here
 });
 
 export default instance;
