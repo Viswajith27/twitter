b0460a6cbae495fc7bfed05b4bd0132da0e0a872
Viswajith Rajan <viswajithrajan@gmail.com>
2023-09-15 21:10:41 +0530

    done

--- Twitter clone/twitter-clone-yt/server/error.js
+++
@@ -1,6 +0,0 @@
-export const handleError = (status, message) => {
-  const error = new Error();
-  error.status = status;
-  error.message = message;
-  return error;
-};
---
+++ Twitter-clone-project/client/src/redux/axiosconfig.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://twitter-clone-sh.vercel.app', // Set your base URL here
+});
+
+export default instance;
+  
