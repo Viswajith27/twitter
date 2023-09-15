f0701db7b751b7e6cac5129b5733f35688d2806c
Viswajith Rajan <viswajithrajan@gmail.com>
2023-09-15 16:41:18 +0530

    Update ExploreTweets.jsx

--- Twitter clone/twitter-clone-yt/client/src/components/ExploreTweets/ExploreTweets.jsx
+++ Twitter clone/twitter-clone-yt/client/src/components/ExploreTweets/ExploreTweets.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import axios from "axios";
+import axios from "../../redux/axiosconfig";
 import { useSelector } from "react-redux";
 import Tweet from "../Tweet/Tweet";
 
