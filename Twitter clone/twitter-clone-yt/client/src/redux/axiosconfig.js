d256f789631c8e56a919b09132c9be477dcd71dc
Viswajith Rajan <viswajithrajan@gmail.com>
2023-09-15 16:48:06 +0530

    Update axiosconfig.js

--- Twitter clone/twitter-clone-yt/client/src/redux/axiosconfig.js
+++ Twitter clone/twitter-clone-yt/client/src/redux/axiosconfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://deploy-mern-api.vercel.app/', // Set your base URL here
+  baseURL: 'https://twitter-clone-sh.vercel.app', // Set your base URL here
 });
 
 export default instance;
